c2f4cf144184082ab512e450dd0f59401dd6f6b4
Roman Belykh <106916877+Roman2444@users.noreply.github.com>
2026-08-30 20:46:17 +0300

    add In-memory  режим

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-const fs = require('fs');
 const jsonServer = require('json-server');
-const path = require('path');
-
-const server = jsonServer.create();const jsonServer = require('json-server');
 const path = require('path');
 
 const server = jsonServer.create();
